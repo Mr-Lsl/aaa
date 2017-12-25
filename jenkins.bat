1e544e5f7fd32c606ea4b04109a3ffbddc5001b2
chaseonesdream <chaseonesdream@163.com>
2017-12-25 11:11:52 +0800

    change

--- jenkins.bat
+++ jenkins.bat
@@ -1,8 +1,12 @@
 @echo off 
-echo "下载package.json中的依赖包已完成"
+echo 开始执行
+echo 下载package.json中的依赖包已完成
+echo 开始进行代码规范检测
 npm run lint
-echo "src目录下代码规范检测完成"
+echo src目录下代码规范检测完成
+echo 开始进行项目打包
 npm run build
-echo "项目打包完成"
+echo 项目打包完成
 cd src
 dir
+echo 构建结束
